39410f2f3da919c7689291f493284f62c87e3730
Igor Lima <igorlima1740@gmail.com>
2024-11-14 13:23:43 +0900

    fix: error in terms of relative path

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 
 cwd = os.getcwd()
-dotenv_path = os.path.join(cwd, os.getenv('ENVIRONMENT_FILE', '.env.development'))
+# dotenv_path = os.path.join(cwd, os.getenv('ENVIRONMENT_FILE', '.env.development'))
+dotenv_path = os.path.join(cwd, os.getenv('ENVIRONMENT_FILE', '.env.production'))
 load_dotenv(dotenv_path=dotenv_path, override=True)
 
 APP_HOST = os.environ.get('HOST')
